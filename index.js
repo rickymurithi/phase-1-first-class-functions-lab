78f784c55247f40599dafe789173401adf2bb452
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-08-23 00:21:00 +0300

    add file

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+// Code!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(select) {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(select) {
+    return drivers.slice(2, 4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(fareMultiplier) {
+    return function(fvalue) {
+        return fareMultiplier * fvalue;
+    }
+}
+
+const fareDoubler = (createFareMultiplier) => (createFareMultiplier)*2;
+
+const fareTripler = (createFareMultiplier) => (createFareMultiplier)*3;
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers);
+}
+    
